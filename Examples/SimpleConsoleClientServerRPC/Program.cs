25396b84926cf1f05c672eb7f47e40c96c436bbb
herve <simon@dufflecoat.io>
2025-09-16 14:04:15 +1000

    simple hello world app

---
+++ Examples/SimpleConsoleClientServerRPC/Program.cs
@@ -0,0 +1,80 @@
+﻿using GTMH.IDL;
+using GTMH.Rabbit;
+using GTMH.Rabbit.RPC;
+
+using RabbitMQ.Client;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+[RPCInterface]
+public interface IHelloWorld
+{
+  [RPCMethod]
+  ValueTask<string> IntroducingAsync(string a_Identity);
+}
+
+public class ServerImpl : IHelloWorld
+{
+  public readonly string Identity;
+
+  public ServerImpl(string a_Identity)
+  {
+    this.Identity = a_Identity;
+  }
+  public ValueTask<string> IntroducingAsync(string a_Identity)
+  {
+    return ValueTask.FromResult($"Hello {a_Identity}, my name is {Identity}");
+  }
+}
+public class HostOnlyFactory(string a_HostName) : IRabbitFactory
+{
+  public string HostIdentity { get; } = a_HostName;
+  public IConnectionFactory Create()=>new ConnectionFactory { HostName = HostIdentity };
+}
+
+public class NullLogger : ILogger
+{
+  public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
+  public bool IsEnabled(LogLevel logLevel) => false;
+  public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) { }
+}
+public static class Program
+{
+  public static async Task Main(string [] args)
+  {
+    var identity = args.FirstOrDefault(arg => arg.StartsWith("--identity="))?.Split('=')[1] ?? "roger";
+    var rabbit = args.FirstOrDefault(arg => arg.StartsWith("--rabbit="))?.Split('=')[1] ?? "localhost";
+    var runas = (args.FirstOrDefault(arg => arg.StartsWith("--runas="))?.Split('=')[1] ?? "client").ToLower();
+
+    Console.WriteLine($"I am {identity} connecting to Rabbit@{rabbit}");
+    if(runas == "client")
+    {
+      await RunClient(identity, new RPCFactory(new HostOnlyFactory(rabbit)));
+    }
+    else
+    {
+      await RunServer(identity, new RPCFactory(new HostOnlyFactory(rabbit)));
+    }
+  }
+
+  private static async ValueTask RunServer(string identity, IRPCFactory rabbit)
+  {
+    Console.WriteLine("Running as server, hit enter to quit...");
+    var serverImpl = new ServerImpl(identity);
+    var serverHost = new HelloWorldDispatch(rabbit, new BasicTopology(), new NullLogger(), serverImpl);
+    var server = await serverHost.Publish();
+    await using(server)
+    {
+      Console.ReadLine();
+    }
+  }
+
+  private static async ValueTask RunClient(string identity, IRPCFactory rabbit)
+  {
+    Console.WriteLine("Running as client...");
+    var client = new HelloWorldClient(rabbit, new BasicTopology(), new NullLogger(), null );
+    await client.Connect();
+    var result = await client.IntroducingAsync(identity);
+    Console.WriteLine($"RPC pleasantries... {result}");
+  }
+}
