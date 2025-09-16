317b06ccd42a8c73df6dab028d7a8d9a9ec6ea47
herve <simon@dufflecoat.io>
2025-09-16 12:52:20 +1000

    codegen copied, builds

---
+++ GTMH.Rabbit.RPC.CodeGen/CommonCodeGen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GTMH.Rabbit.RPC.CodeGen
+{
+  public class CommonCodeGen
+  {
+    public struct AsyncReturnType
+    {
+      public bool IsAsync { get; set; }
+      public string Type { get; set; }
+      public static AsyncReturnType Synchronous => new AsyncReturnType { IsAsync = false };
+      public static AsyncReturnType Asynchronous(string a_Type) => new AsyncReturnType { IsAsync = true, Type = a_Type };
+    }
+    public static AsyncReturnType IsAsyncReturnType(string a_Type)
+    {
+      // not a compiler !
+      if ( ! a_Type.Contains("Task")) return AsyncReturnType.Synchronous;
+      var reTask = new Regex(@"^System.Threading.Tasks.Task<(.*)>");
+      var reValueTask = new Regex(@"^System.Threading.Tasks.ValueTask<(.*)>");
+      var reVoidTask = new Regex("^System.Threading.Tasks.Task$");
+      var reVoidValueTask = new Regex("^System.Threading.Tasks.ValueTask$");
+      var m = reTask.Match(a_Type);
+      if(m.Success)
+      {
+        return AsyncReturnType.Asynchronous(m.Groups[1].Value);
+      }
+      m = reValueTask.Match(a_Type);
+      if(m.Success)
+      {
+        return AsyncReturnType.Asynchronous(m.Groups[1].Value);
+      }
+      if ( reVoidTask.IsMatch(a_Type) ) return AsyncReturnType.Asynchronous(null);
+      if ( reVoidValueTask.IsMatch(a_Type) ) return AsyncReturnType.Asynchronous(null);
+      return AsyncReturnType.Synchronous;
+    }
+  }
+}
---
+++ GTMH.Rabbit.RPC.CodeGen/InterfaceDefn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTMH.Rabbit.RPC.CodeGen
+{
+  public readonly struct MethodArg
+  {
+    public readonly string Type;
+    public readonly string Name;
+    public MethodArg(string a_Type, string a_Name)
+    {
+      Type=a_Type;
+      Name = a_Name;
+    }
+  }
+  public readonly struct MethodDefn
+  {
+    public readonly string Name;
+    public readonly string Type;
+    public readonly List<MethodArg> Args;
+    public MethodDefn(string a_Type, string a_Name, List<MethodArg> a_Args)
+    {
+      Name =a_Name;
+      Type = a_Type;
+      Args = a_Args;
+    }
+  }
+
+  public readonly struct InterfaceDefn
+  {
+    public readonly string Name;
+    public readonly string Namespace;
+    public readonly List<string> Usings;
+    public readonly List<MethodDefn> Methods;
+    public InterfaceDefn(string a_Name, string a_NS, List<string> a_Usings, List<MethodDefn> a_Methods)
+    {
+      Name = a_Name;
+      Namespace = a_NS;
+      Usings = a_Usings;
+      Methods = a_Methods;
+    }
+
+    internal string GetImplClassName(string a_Suffix)
+    {
+      if(Name.StartsWith("I"))
+      {
+        return $"{Name.Substring(1)}{a_Suffix}";
+      }
+      else
+      {
+        return $"{Name}{a_Suffix}";
+      }
+    }
+  }
+}
