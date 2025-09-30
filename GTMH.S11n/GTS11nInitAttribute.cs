e997f82a938fb4e1432006b756202184b7569ab9
herve <simon@dufflecoat.io>
2025-09-30 11:33:21 +1000

    bye

--- GTMH.S11n/GTS11nInitAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GTMH.S11n
-{
-	[AttributeUsage(AttributeTargets.Constructor)]
-  public class GTS11nInitAttribute : Attribute { }
-}
